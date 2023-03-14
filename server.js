bd8dc111ee3fbec4d977f72a7d66f7320911e44c
Djeff <djeff.walla99@gmail.com>
2023-03-13 21:26:07 -0300

    fix: port-3002

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
